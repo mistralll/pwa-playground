fd535e99fb146b968346826470179d00d87bb055
mistralll <me.pooh441@gmail.com>
2022-06-01 14:10:41 +0900

    add: func

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,9 @@
-const myHeading = documet.querySelector('h1');
-myHeading.textContent = 'Hello Wrold';
+function myfunc() {
+    msg.innerText = '内容は' + txt.value + 'です。';
+}
 
+let txt = document.getElementById('t01');
+let msg = document.getElementById('msg');
+let btn = document.getElementById('b01');
+
+btn.addEventListener('click', myfunc);
